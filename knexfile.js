c039e6309eee4bc9a3ea2212099b9a2b185c9349
Luis Hernandez <luishrd@gmail.com>
2017-12-11 12:53:27 -0700

    Adds solution code

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/blogdb.sqlite3' }, // change this if you want a different name for the database
+    useNullAsDefault: true,
+    migrations: { tableName: 'dbmigrations' },
+    seeds: { directory: './database/seeds' },
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost', // update this
+      user: 'put your username here', // update this with the user you use to connect to MySQL
+      password: 'pass', // update this with the password of the user you use to connect to MySQL
+      database: 'blogdb', // if you want to use a different database change this name
+    },
+    pool: {
+      min: 1,
+      max: 10,
+    },
+    migrations: { tableName: 'dbmigrations' },
+    seeds: { directory: './database/seeds' },
+  },
+};
